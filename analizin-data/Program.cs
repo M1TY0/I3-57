850b4a5cf0eb5b8a8391a12ae212143a80ac7b08
M1TY0 <100305309+M1TY0@users.noreply.github.com>
2022-12-06 15:48:23 +0200

    85% done

---
+++ analizin-data/Program.cs
@@ -0,0 +1,55 @@
+﻿Dictionary<string, string> months = new Dictionary<string, string>(){
+    {"1","January " },
+
+
+     {"2","February"},
+    {"3" , "March"},
+     {"4" , "April"},
+      {"5" , "May"},
+       {"6" , "June"},
+        {"7" , "July"},
+         {"8" , "August"},
+          {"9" , "September"},
+           {"10" , "Octomber"},
+            {"11" , "November"},
+             {"12" , "Desember"}
+
+};
+int[] n = new int[12];
+for (int i = 0; i < n.Length; i++)
+{
+    n[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine(n.Average());
+Console.WriteLine(months[(indexMax(n).ToString())] + " " + n.Max()) ;
+Console.WriteLine(months[(indexMin(n).ToString())] + " " + n.Min());
+
+
+int indexMin(int[] n)
+{
+    int i = 0;
+    int x = n.Min();
+    for (int f = 0; f < n.Length; f++)
+    {
+        if (n[f] == x)
+        {
+            i = f;
+            break;
+        }
+    }
+    return i;
+}
+int indexMax (int[] n)
+    {
+    int i = 0;
+    int x = n.Max();
+    for (int f = 0; f < n.Length; f++)
+    {
+        if (n[f] == x)
+        {
+            i = f;
+            break;
+        }
+    }
+    return i;
+}
